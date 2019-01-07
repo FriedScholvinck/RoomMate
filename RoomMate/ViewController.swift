f4a62c360939f8661255f5f9153eba28a7b7ad56
Fried Scholvinck <fried.scholvinck@gmail.com>
2019-01-07 16:45:49 +0100

    log in with firebase + tab bar controller

--- RoomMate/ViewController.swift
+++ RoomMate/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        titleLabel.applyDesign()
+        subtitleLabel.applyDesign()
+        loginLabel.applyDesign()
+        loginButton.applyDesign()
     }
 
-
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
+        
+        // get default auth UI object
+        let authUI = FUIAuth.defaultAuthUI()
+        guard authUI != nil else {
+            return
+        }
+        
+        // set ourselves as the delegate
+        authUI?.delegate = self
+        
+        // get reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        
+        // show controller
+        present(authViewController, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: FUIAuthDelegate {
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if error != nil {
+            return
+        }
+        
+        // get user uid
+        //authDataResult?.user.uidr
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+}
+
+extension UILabel {
+    func applyDesign() {
+        self.backgroundColor = UIColor(red:0.22, green:0.57, blue:0.47, alpha:1.0)
+        self.layer.cornerRadius = self.frame.height / 2
+        self.textColor = UIColor.white
+    }
+}
+
+extension UIButton {
+    func applyDesign() {
+        self.backgroundColor = UIColor(red:0.22, green:0.57, blue:0.47, alpha:1.0)
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = self.frame.height / 2
+        self.setTitleColor(UIColor.white, for: .normal)
+    }
+}
